d26fda449c5f2a426f6803217ca9cab5489db925
Patrício Samuel <patricioanjosjf@gmail.com>
2025-05-07 07:45:51 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd"
+
+function App() {
+
+  return (
+    <>
+      <div>Hello</div>
+      <Button type="primary" onClick={() => alert('Hello!')}>Primary Button</Button>
+    </>
+  )
+}
+
+export default App
